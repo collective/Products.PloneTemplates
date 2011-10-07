718d41def72189c9feaf304e43a5988c6a6b4f75
Hanno Schlichting <hanno@hannosch.eu>
2011-10-07 08:42:24 +0000

    Update metadata
    git-svn-id: http://svn.plone.org/svn/collective/Products.PloneTemplates/trunk@245241 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -1,37 +1,27 @@
-from setuptools import setup
-
-
-CLASSIFIERS = [
-    'Programming Language :: Python',
-    'Framework :: Zope2',
-    'Framework :: Plone',
-]
+from setuptools import setup, find_packages
 
 version = '1.0.3'
 
-readme_file = 'README.txt'
-desc = open(readme_file).read().strip()
-changes_file = 'CHANGES.txt'
-changes = open(changes_file).read().strip()
-
-long_description = desc + '\n\nCHANGES\n=======\n\n' +  changes 
-
 setup(name='Products.PloneTemplates',
       version=version,
-      license='ZPL',
+      description='Content templates for Plone',
+      long_description=open('README.txt').read() + '\n' +
+                       open('CHANGES.txt').read(),
       author='Danny Bloemendaal',
       author_email='danny.bloemendaal@informaat.nl',
-      maintainer='Danny Bloemendaal',
-      maintainer_email='danny.bloemendaal@informaat.nl',
-      classifiers=CLASSIFIERS,
+      classifiers=[
+          'Programming Language :: Python',
+          'Framework :: Zope2',
+          'Framework :: Plone',
+      ],
       keywords='Zope Plone Templates', 
+      license='ZPL',
       url='http://pypi.python.org/pypi/Products.PloneTemplates',
-      description='Content templates for Plone',
-      long_description=long_description,
-      packages=['Products', 'Products.PloneTemplates'],
-      include_package_data = True,
-      zip_safe=False,
-      install_requires=['setuptools'],
+      packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
-
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+      ],
       )
